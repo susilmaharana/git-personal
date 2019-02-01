af243efa21fef6efc2eaa28d19d9822fd97c27ed
Susil Kumar Maharana (HCL AMERICA INC) <v-sumah@microsoft.com>
2019-02-01 02:37:10 -0800

    update done

--- Calculator/using-jquery/scripts/main.js
+++ Calculator/using-jquery/scripts/main.js
@@ -1,3 +1,51 @@
 $(function(){
-    
+    $('.key-row div span').each(function(){
+        $(this).parent().attr("data-key-val", $(this).text());
+    });
+
+    var calculate = function() {
+        //calculate whatever inside action-trail
+    }
+
+    var appendValues = function(keyVal) {
+        $('#action-trail').append(this);
+    } 
+
+    $('.key-row div').on('click', function(){
+        var keyVal = $(this).data('key-val');
+        switch(keyVal) {
+            case '1': 
+            case '2': 
+            case '3': 
+            case '4': 
+            case '5': 
+            case '6': 
+            case '7': 
+            case '8': 
+            case '9': 
+            case '0': 
+            case '.' :
+                appendValues(keyVal);
+                break;
+            case '/':
+            case '*':
+            case '-':
+            case '+':
+                appendValues(keyVal);
+                break;
+            case '=':
+                calculate();
+                break;
+            default:
+                break;
+                
+        }
+        if($(this).text() !== '=')
+        {
+            $('#action-trail').append($(this).data('key-val'));
+        }
+        else {
+            calculate();
+        }
+    });
 });
